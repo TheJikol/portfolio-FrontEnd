3a78364379e3fae04a6d9dc4c0fb7eb1ff88fe98
Jeremías <jeremias.elian@gmail.com>
2022-09-30 20:36:34 -0300

    Education, projects, contact and stars created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BiographyComponent } from './components/biography/biography.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { MoonflipComponent } from './components/moonflip/moonflip.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BiographyComponent,
-    ExperienceComponent
+    ExperienceComponent,
+    EducationComponent,
+    ProjectsComponent,
+    MoonflipComponent
   ],
   imports: [
     BrowserModule,
