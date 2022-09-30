1bf5f216da10a3b1e6a80cb0a7d8693245a16f5c
Jeremías <jeremias.elian@gmail.com>
2022-09-29 22:58:06 -0300

    experiencie component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BiographyComponent } from './components/biography/biography.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BiographyComponent
+    BiographyComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
